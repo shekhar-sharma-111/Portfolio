132ab7741949af99b1fe6aae58eddfa45bb0da42
Shekhar Sharma <139949866+shekhar-sharma-111@users.noreply.github.com>
2024-07-06 00:01:31 +0530

    Update Skills.jsx

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -2,17 +2,27 @@
 import React from 'react';
 
 const skills = [
-  { name: 'HTML', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1280px-HTML5_logo_and_wordmark.svg.png', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-  { name: 'CSS', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/1280px-CSS3_logo.svg.png', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-  { name: 'JavaScript', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/1280px-JavaScript-logo.png', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-  { name: 'React', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png', link: 'https://reactjs.org/' },
-  { name: 'Node.js', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png', link: 'https://nodejs.org/' },
-  { name: 'MongoDB', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc6nYbJje18p5tPU2DEHDxcXncvrVpHwvfwdfdIeCpIQDfpzaqZc6890o&s=10', link: 'https://www.mongodb.com/' },
-  { name: 'Express', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/1280px-Expressjs.png', link: 'https://expressjs.com/' },
-  { name: 'C++', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQs86jDXWZu_DQ5-r-CXcGq7vdXZ4neXSqEW8CJJPL2a_MjOlCUnNPVKyf&s=10', link: 'https://en.wikipedia.org/wiki/C%2B%2B' },
-  { name: 'Bootstrap', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png', link: 'https://getbootstrap.com/' },
-  { name: 'Git', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png', link: 'https://git-scm.com/' },
-];
+    { name: 'HTML', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1280px-HTML5_logo_and_wordmark.svg.png' },
+  { name: 'CSS', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/1280px-CSS3_logo.svg.png' },
+  { name: 'JavaScript', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/1280px-JavaScript-logo.png' },
+  { name: 'React', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'},
+  { name: 'Node.js', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png' },
+  { name: 'MongoDB', logo: 'https://1000logos.net/wp-content/uploads/2020/08/MongoDB-Logo.png' },
+  { name: 'Express', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/1280px-Expressjs.png' },
+  { name: 'C++', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZNYG08hKj5oCT0N34qwh37dAw6LJtnCf3HQ&s' },
+  { name: 'Bootstrap', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png'},
+  { name: 'Git', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png' },
+//   { name: 'HTML', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1280px-HTML5_logo_and_wordmark.svg.png', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+//   { name: 'CSS', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/1280px-CSS3_logo.svg.png', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+//   { name: 'JavaScript', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/1280px-JavaScript-logo.png', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+//   { name: 'React', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png', link: 'https://reactjs.org/' },
+//   { name: 'Node.js', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png', link: 'https://nodejs.org/' },
+//   { name: 'MongoDB', logo: 'https://1000logos.net/wp-content/uploads/2020/08/MongoDB-Logo.png', link: 'https://www.mongodb.com/' },
+//   { name: 'Express', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/1280px-Expressjs.png', link: 'https://expressjs.com/' },
+//   { name: 'C++', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZNYG08hKj5oCT0N34qwh37dAw6LJtnCf3HQ&s', link: 'https://en.wikipedia.org/wiki/C%2B%2B' },
+//   { name: 'Bootstrap', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png', link: 'https://getbootstrap.com/' },
+//   { name: 'Git', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png', link: 'https://git-scm.com/' },
+// ];
 
 export default function Skills() {
   return (
